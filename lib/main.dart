0f0989e719aa992c9583a6e3ac6e0ea24241c1a7
RuwanUdayanga <ruwanudayanga01@gmail.com>
2023-07-11 23:15:37 +0530

    Update home page.

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,17 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+  var favorites = <WordPair> {};
 
+  void toogleFavorite(){
+    if(favorites.contains(current)){
+      favorites.remove(current);
+    }
+    else{
+      favorites.add(current);
+    }
+    notifyListeners();
+  }
   void getNext() {
     current = WordPair.random();
     notifyListeners();
@@ -42,6 +52,13 @@ class MyHomePage extends StatelessWidget {
     var appState = context.watch<MyAppState>();
     var pair = appState.current;
 
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+
     return Scaffold(
       body: Center(
         child: Column(
@@ -49,11 +66,25 @@ class MyHomePage extends StatelessWidget {
           children: [
             BigCard(pair: pair),
             SizedBox(height: 15,),
-            ElevatedButton(
-              onPressed: () {
-                appState.getNext();
-              },
-              child: Text('Next'),
+
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ElevatedButton.icon(
+                    onPressed: (){
+                      appState.toogleFavorite();
+                    },
+                    icon: Icon(icon),
+                    label: Text('Like')
+                ),
+                SizedBox(width: 15,),
+                ElevatedButton(
+                  onPressed: () {
+                    appState.getNext();
+                  },
+                  child: Text('Next'),
+                ),
+              ],
             ),
 
           ],
@@ -82,7 +113,7 @@ class BigCard extends StatelessWidget {
       color: theme.colorScheme.primary,
       child: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Text(pair.asLowerCase, style: style, semanticsLabel: pair.asPascalCase,),
+        child: Text(pair.asLowerCase, style: style, semanticsLabel: "${pair.first} ${pair.second}",),
       ),
     );
   }
