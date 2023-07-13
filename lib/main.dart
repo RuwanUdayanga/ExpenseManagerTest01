5efe391a48b8535d96a51725f887d10151399c43
RuwanUdayanga <ruwanudayanga01@gmail.com>
2023-07-13 18:24:13 +0530

    Add Favorites page and History.

--- lib/main.dart
+++ lib/main.dart
@@ -27,14 +27,16 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
-  var favorites = <WordPair> {};
+  var favorites = <WordPair> [];
+  var history = <WordPair> [];
 
-  void toogleFavorite(){
+  void toggleFavorite(){
     if(favorites.contains(current)){
       favorites.remove(current);
     }
     else{
       favorites.add(current);
+      history.add(current);
     }
     notifyListeners();
   }
@@ -42,11 +44,85 @@ class MyAppState extends ChangeNotifier {
     current = WordPair.random();
     notifyListeners();
   }
-
+  void deleteFavorite(int index) {
+    favorites.removeAt(index);
+    notifyListeners();
+  }
+  void deleteHistory(int index) {
+    history.removeAt(index);
+    notifyListeners();
+  }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = GeneratorPage();
+        break;
+      case 1:
+        page = FavoritesPage();
+        break;
+      case 2:
+        page = History();
+        break;
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+    }
+
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Scaffold(
+          body: Row(
+            children: [
+              SafeArea(
+                child: NavigationRail(
+                  extended: constraints.maxWidth >= 600,
+                  destinations: [
+                    NavigationRailDestination(
+                      icon: Icon(Icons.home),
+                      label: Text('Home'),
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.favorite),
+                      label: Text('Favorites'),
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.history),
+                      label: Text('history'),
+                    ),
+                  ],
+                  selectedIndex: selectedIndex,
+                  onDestinationSelected: (value) {
+                    setState(() {
+                      selectedIndex = value;
+                    });
+                  },
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Theme.of(context).colorScheme.primaryContainer,
+                  child: page,
+                ),
+              ),
+            ],
+          ),
+        );
+      }
+    );
+  }
+}
+
+class GeneratorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
@@ -59,36 +135,32 @@ class MyHomePage extends StatelessWidget {
       icon = Icons.favorite_border;
     }
 
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            BigCard(pair: pair),
-            SizedBox(height: 15,),
-
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                ElevatedButton.icon(
-                    onPressed: (){
-                      appState.toogleFavorite();
-                    },
-                    icon: Icon(icon),
-                    label: Text('Like')
-                ),
-                SizedBox(width: 15,),
-                ElevatedButton(
-                  onPressed: () {
-                    appState.getNext();
-                  },
-                  child: Text('Next'),
-                ),
-              ],
-            ),
-
-          ],
-        ),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BigCard(pair: pair),
+          SizedBox(height: 10),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleFavorite();
+                },
+                icon: Icon(icon),
+                label: Text('Like'),
+              ),
+              SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: Text('Next'),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
@@ -115,6 +187,104 @@ class BigCard extends StatelessWidget {
         padding: const EdgeInsets.all(20.0),
         child: Text(pair.asLowerCase, style: style, semanticsLabel: "${pair.first} ${pair.second}",),
       ),
+    );
+  }
+}
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favorites.isEmpty) {
+      return Center(
+        child: Text('No favorites yet.'),
+      );
+    }
+
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('You have '
+              '${appState.favorites.length} favorites:',
+              style: TextStyle(
+              color: Colors.red,
+              fontSize:16,
+              fontWeight: FontWeight.bold,
+              fontStyle: FontStyle.normal,
+          ),
+          ),
+        ),
+        for (var index = 0; index < appState.favorites.length; index++)
+          ListTile(
+            leading: Icon(Icons.favorite_outline_sharp),
+            title: Text(appState.favorites[index].asLowerCase),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(
+                    Icons.delete,
+                    size: 20.0,
+                    color: Colors.brown[900],
+                  ),
+                  onPressed: () {
+                    appState.deleteFavorite(index);
+                  },
+                ),
+              ],
+
+            ),
+          ),
+      ],
+    );
+  }
+}
+class History extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favorites.isEmpty) {
+      return Center(
+        child: Text('History is empty.'),
+      );
+    }
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('History',style: TextStyle(
+            color: Colors.blue,
+            fontSize: 26,
+            fontWeight: FontWeight.bold,
+            fontStyle: FontStyle.normal,
+          ),
+          ),
+        ),
+        for (var indexh = 0; indexh < appState.history.length; indexh++)
+          ListTile(
+            leading: Icon(Icons.history),
+            title: Text(appState.history[indexh].asLowerCase),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(
+                    Icons.delete,
+                    size: 20.0,
+                    color: Colors.brown[900],
+                  ),
+                  onPressed: () {
+                    appState.deleteHistory(indexh);
+                  },
+                ),
+              ],
+
+            ),
+          ),
+      ],
     );
   }
 }
